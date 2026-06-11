f61476cfda7a326753ed78c0c3fb98cd8bcd07e1
unknown <mutheedavid12@gmail.com>
2026-06-11 10:50:35 +0300

    feat: learn async session and database connection

--- app/database/session.py
+++ app/database/session.py
@@ -1,25 +1,34 @@
 from typing import Annotated
+
 from fastapi import Depends
-from sqlalchemy import create_engine
-from sqlmodel import SQLModel, Session
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
 
+from app.config import settings
 
-engine = create_engine(
-    url="sqlite:///sqlite.db",
+engine = create_async_engine(
+    url=settings.POSTGRES_URL,
     echo=True,
-    connect_args={"check_same_thread": False},
 )
 
 
-def create_db_tables():
-    from app.schemas import Shipment
+async def create_db_tables():
+    async with engine.begin() as connection:
+        from .models import Shipment
 
-    SQLModel.metadata.create_all(bind=engine)
+        await connection.run_sync(SQLModel.metadata.create_all)
 
 
-def get_session():
-    with Session(bind=engine) as session:
+async def get_session():
+    async_session = sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
+
+    async with async_session(bind=engine) as session:
         yield session
 
 
-SessionDep = Annotated[Session, Depends(get_session)]
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
